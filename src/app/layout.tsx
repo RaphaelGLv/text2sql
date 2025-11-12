829d5585f70205f911ba18b275cd5c1facbdf6ec
Raphael Gonçalves Leiva <raphael.leiva@rubcube.com>
2025-11-12 19:53:21 -0300

    feat(wip): implement toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import styles from "./layout.module.css";
+import { Toast } from "./components/toast/toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${styles.body} ${geistSans.variable} ${geistMono.variable}`}
       >
+        <Toast />
         {children}
       </body>
     </html>
