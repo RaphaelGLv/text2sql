878e936766c5ea7ca05a7234da49f78a64757167
Raphael Gonçalves Leiva <raphael.leiva@rubcube.com>
2025-11-17 11:53:18 -0300

    feat: implement metadata

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,6 +1,11 @@
 import styles from "./page.module.css";
 import { DbSchemasList } from "../../components/ui/db-schemas-list/db-schemas-list";
 import { BaseScreen } from "../../components/layout/base-screen/base-screen";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Text2SQL",
+};
 
 export default function Home() {
   return (
--- src/app/(routes)/chat/[id]/page.tsx
+++ src/app/(routes)/chat/[id]/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import { ChatClient } from "./chat-client";
 import { BaseScreen } from "@/app/components/layout/base-screen/base-screen";
+
+export const metadata: Metadata = {
+  title: "Chat | Text2SQL"
+}
 
 export default function Page() {
   return (
